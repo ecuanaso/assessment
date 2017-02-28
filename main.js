9411677822c0ec635b665eff288955e207a24ef0
David Castillo <miestilo23@gmail.com>
2017-02-27 23:03:08 -0500

    Updated labels to display numbers

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ $(function(){
 	// Create arrays
 	var arr = [];
 	var labels = [
-		"Red", "Blue", "Yellow", "Green", "Purple", "Orange",
-		"Red", "Blue", "Yellow", "Green", "Purple", "Orange", 
-		"Red", "Blue", "Yellow", "Green", "Purple", "Orange", 
-		"Red", "Blue", "Yellow", "Green", "Purple", "Orange", 
-		"Red", "Blue", "Yellow", "Green", "Purple", "Orange", 
-		"Red", "Blue", "Yellow", "Green", "Purple", "Orange", 
-		"Red", "Blue", "Yellow", "Green", "Purple", "Orange", 
-		"Red", "Blue", "Yellow", "Green", "Purple", 
+		"1", "2", "3", "4", "5", "6",
+		"7", "8", "9", "10", "11", "12", 
+		"13", "14", "15", "16", "17", "18", 
+		"19", "20", "21", "22", "23", "24", 
+		"25", "26", "27", "28", "29", "30", 
+		"31", "32", "33", "34", "35", "36", 
+		"37", "38", "39", "40", "41", "42", 
+		"43", "44", "45", "46", "47", 
 	];
 
 	var bkgColor = [
